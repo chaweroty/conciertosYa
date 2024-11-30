1166876dca2b84b014daba33c28b8988ab336f7d
Danielesp19 <danielespitia369@gmail.com>
2024-11-30 18:40:02 -0500

    Se implemento el cambio de vista del login al register y ya quedo conectado el formulario del frontend con nuestro backend, permitiendo crear usuarios desde el front

--- frontend/src/components/auth/LoginPage.jsx
+++ frontend/src/components/auth/LoginPage.jsx
@@ -28,15 +28,25 @@ function LoginPage() {
       }, 5000);
     }
   };
+  const handleRegisterClick = () => {
+    navigate("/register"); // Cambia "/register" por la ruta de tu página de registro.
+  };
 
   return (
     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
       <div className="auth-container">
-        <h2 className="text-2xl font-bold tracking-tight text-gray-900">Sign in to your account</h2>
+        <h2 className="text-2xl font-bold tracking-tight text-gray-900">
+          Sign in to your account
+        </h2>
         {error && <p className="error-message">{error}</p>}
         <form onSubmit={handleSubmit} className="space-y-6">
           <div className="form-group">
-            <label htmlFor="email" className="block text-sm font-medium text-gray-900">Email address</label>
+            <label
+              htmlFor="email"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Email address
+            </label>
             <input
               id="email"
               name="email"
@@ -48,7 +58,12 @@ function LoginPage() {
             />
           </div>
           <div className="form-group">
-            <label htmlFor="password" className="block text-sm font-medium text-gray-900">Password</label>
+            <label
+              htmlFor="password"
+              className="block text-sm font-medium text-gray-900"
+            >
+              Password
+            </label>
             <input
               id="password"
               name="password"
@@ -59,11 +74,23 @@ function LoginPage() {
               className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm"
             />
           </div>
-          <button type="submit" className="flex justify-center w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <button
+            type="submit"
+            className="flex justify-center w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
             Sign in
           </button>
-          <label htmlFor="email" className="block text-sm font-medium text-gray-900">Si no tienes una cuenta, te puedes registrar!</label>
-          <button type="submit" className="flex justify-center w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+          <label
+            htmlFor="email"
+            className="block text-sm font-medium text-gray-900"
+          >
+            Si no tienes una cuenta, te puedes registrar!
+          </label>
+          <button
+            type="button"
+            onClick={handleRegisterClick}
+            className="flex justify-center w-full rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+          >
             Register
           </button>
         </form>
